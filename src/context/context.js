2b43f8c1cd14afa0449cb43f53aac313773a58d5
Naveenprasanth T <102594021+NaveenprasanthT@users.noreply.github.com>
2022-07-08 19:49:18 +0530

    Update context.js

--- src/context/context.js
+++ src/context/context.js
@@ -2,7 +2,7 @@ import React, {useReducer, createContext } from 'react';
 import contextReducer from './contextReducer';
 
 
-const initialState = JSON.parse(localStorage.getItem('transactions'));
+const initialState = JSON.parse(localStorage.getItem('transactions'))  || [{type: "Expense", category: "Clothes", amount: 1000, date: "2022-07-08"},{type: "Income", category: "Investments", amount: 5000, date: "2022-07-08"}];
 export const ExpenceTracker = createContext(initialState);
 
 export const Provider = ({children}) =>{
