f5e04fdc66649201247638444a4a0975bea97cb2
Naveenprasanth T <102594021+NaveenprasanthT@users.noreply.github.com>
2022-07-13 20:26:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,14 +5,28 @@ import Main from './components/Main/Main.jsx';
 import {PushToTalkButton, PushToTalkButtonContainer} from '@speechly/react-ui';
 
 const App = () =>{
+
+    const [width, setWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => setWidth(window.innerWidth);
+        window.addEventListener("resize", handleResize);
+        return () => {
+          window.removeEventListener("resize", handleResize);
+        };
+      });
+
     return(
         <div>
            <Grid container spacing={2} alignItems='center' justifyItems='center' style={{height: '100vh'}}>
-                <Grid item xs={12} sm={4}>
+                <Grid item xs={12} sm={4} sx={width < 600 ? {display:'none'}:{}}>
                     <Details title='Income'/>
                 </Grid>
                 <Grid item xs={12} sm={4}>
                     <Main />
+                </Grid>
+                <Grid item xs={12} sm={4} sx={width > 600 ? {display:'none'}:{}}>
+                    <Details title='Income'/>
                 </Grid>
                 <Grid item xs={12} sm={4}>
                     <Details title='Expense'/>
